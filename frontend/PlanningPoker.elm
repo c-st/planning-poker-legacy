085fe06459d504e835423703e4c995f75de2965a
Christian Stangier <mail@christian-stangier.com>
2016-08-08 20:55:07 +0200

    add routing and link roomId to url parameter

--- frontend/PlanningPoker.elm
+++ frontend/PlanningPoker.elm
@@ -1,20 +1,24 @@
 module Main exposing (..)
 
 import Html exposing (..)
-import Html.App as App
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import RouteUrl.Builder as Builder exposing (Builder, builder, path, replacePath)
+import RouteUrl exposing (UrlChange)
+import Navigation exposing (Location)
 import WebSocket
 import Json.Decode as JD exposing ((:=))
 
 
 main : Program Never
 main =
-    App.program
+    RouteUrl.program
         { init = init
-        , view = view
         , update = update
         , subscriptions = subscriptions
+        , view = view
+        , delta2url = delta2url
+        , location2messages = url2messages
         }
 
 
@@ -27,13 +31,20 @@ planningPokerServer =
 -- model
 
 
+type Page
+    = LandingPage
+    | PlanningPokerRoom
+
+
 type alias User =
     { name : String
     }
 
 
 type alias Model =
-    { input : String
+    { activePage : Page
+    , roomId : String
+    , input : String
     , messages : List String
     , users : List User
     }
@@ -41,7 +52,7 @@ type alias Model =
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model "" [] [], Cmd.none )
+    ( Model LandingPage "" "" [] [], Cmd.none )
 
 
 
@@ -51,6 +62,8 @@ init =
 type Msg
     = Input String
     | Send
+    | SetRoomId String
+    | JoinRoom
     | IncomingEvent String
       -- is being decoded and mapped to these:
     | UnexpectedPayload String
@@ -68,43 +81,55 @@ containsUser users user =
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg { input, messages, users } =
+update msg model =
     case msg of
         Input newInput ->
-            ( Model newInput messages users, Cmd.none )
+            ( { model
+                | input = newInput
+              }
+            , Cmd.none
+            )
 
         Send ->
-            ( Model "" messages users, WebSocket.send planningPokerServer input )
+            ( model, WebSocket.send planningPokerServer model.input )
 
+        SetRoomId newRoomId ->
+            ( { model
+                | roomId = newRoomId
+              }
+            , Cmd.none
+            )
+
+        JoinRoom ->
+            ( model, Cmd.none )
+
+        -- reestablish connection
         IncomingEvent payload ->
             let
-                nextModel =
-                    (Model input messages users)
-
                 nextMessage =
                     decodePayload payload
             in
-                update nextMessage nextModel
+                update nextMessage model
 
         UnexpectedPayload message ->
-            ( Model input messages users, Cmd.none )
+            ( model, Cmd.none )
 
         UserJoined user ->
             let
                 newUsers =
-                    if containsUser users user then
-                        users
+                    if containsUser model.users user then
+                        model.users
                     else
-                        user :: users
+                        user :: model.users
             in
-                ( Model input messages newUsers, Cmd.none )
+                ( { model | users = newUsers }, Cmd.none )
 
         UserLeft user ->
             let
                 newUsers =
-                    List.filter (\u -> u.name /= user.name) users
+                    List.filter (\u -> u.name /= user.name) model.users
             in
-                ( Model input messages newUsers, Cmd.none )
+                ( { model | users = newUsers }, Cmd.none )
 
 
 
@@ -147,13 +172,48 @@ subscriptions model =
 
 
 
+-- routing
+
+
+delta2url : Model -> Model -> Maybe UrlChange
+delta2url previous current =
+    Maybe.map Builder.toUrlChange <|
+        delta2builder previous current
+
+
+delta2builder : Model -> Model -> Maybe Builder
+delta2builder previous current =
+    builder
+        |> replacePath [ current.roomId ]
+        |> Just
+
+
+url2messages : Location -> List Msg
+url2messages location =
+    builder2messages (Builder.fromUrl location.href)
+
+
+builder2messages : Builder -> List Msg
+builder2messages builder =
+    case path builder of
+        first :: rest ->
+            [ SetRoomId first ]
+
+        _ ->
+            []
+
+
+
 -- view
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ input [ onInput Input, value model.input ] []
+        [ input [ onInput SetRoomId, value model.roomId ] []
+        , button [ onClick JoinRoom ] [ text "Join room" ]
+        , h3 [] [ text ("roomId: " ++ model.roomId) ]
+        , input [ onInput Input, value model.input ] []
         , button [ onClick Send ] [ text "Send" ]
         , ul [] (List.map viewUser model.users)
         ]
@@ -162,3 +222,8 @@ view model =
 viewUser : User -> Html msg
 viewUser user =
     li [] [ text user.name ]
+
+
+title : String
+title =
+    "Planning poker"
