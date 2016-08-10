28ef654e44f94a9acf654c5d963431a7b9ab947c
Christian Stangier <mail@christian-stangier.com>
2016-08-10 21:41:30 +0200

    use userName consistently

--- backend/src/main/scala/domain/PokerEvents.scala
+++ backend/src/main/scala/domain/PokerEvents.scala
@@ -11,11 +11,11 @@ object SystemMessage {
 sealed trait PokerEvent
 case class IncomingMessage(sender: String, message: String) extends PokerEvent
 
-case class UserJoined(name: String, userActor: ActorRef, eventType: String = "userJoined") extends PokerEvent
-case class UserLeft(name: String, eventType: String = "userLeft") extends PokerEvent
+case class UserJoined(userName: String, userActor: ActorRef, eventType: String = "userJoined") extends PokerEvent
+case class UserLeft(userName: String, eventType: String = "userLeft") extends PokerEvent
 
-case class RequestStartEstimation(sender: String, taskName: String, eventType: String = "startEstimation") extends PokerEvent
+case class RequestStartEstimation(userName: String, taskName: String, eventType: String = "startEstimation") extends PokerEvent
 case class UserEstimate(userName: String, taskName: String, estimate: String, eventType: String = "estimate") extends PokerEvent
 case class UserHasEstimated(userName: String, taskName: String, eventType: String = "userHasEstimated") extends PokerEvent
-case class RequestShowEstimationResult(sender: String, eventType: String = "showResult") extends PokerEvent
+case class RequestShowEstimationResult(userName: String, eventType: String = "showResult") extends PokerEvent
 case class EstimationResult(taskName: String, estimates: Map[String, String], eventType: String = "estimationResult") extends PokerEvent
