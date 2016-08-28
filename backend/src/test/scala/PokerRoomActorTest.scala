f47019d3abf1b0de06edbd07ff1c81125f4c679b
Christian Stangier <mail@christian-stangier.com>
2016-08-28 20:05:18 +0200

    add more tests for initiating estimations

--- backend/src/test/scala/PokerRoomActorTest.scala
+++ backend/src/test/scala/PokerRoomActorTest.scala
@@ -1,6 +1,6 @@
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.testkit.{TestActors, TestKit}
-import domain.{PokerRoomActor, UserJoined, UserLeft}
+import domain._
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 import akka.testkit.TestProbe
 
@@ -10,17 +10,15 @@ class PokerRoomActorTest
   extends TestKit(ActorSystem("test-system"))
     with WordSpecLike with Matchers with BeforeAndAfterAll{
 
-  var roomRef: ActorRef = system.actorOf(Props(classOf[PokerRoomActor], "test-room"))
-
   override def afterAll = {
     shutdown()
   }
 
   "User handling" should {
-    roomRef = system.actorOf(Props(classOf[PokerRoomActor], "test-room"))
-    val probe1 = TestProbe()
-    val probe2 = TestProbe()
-    val probe3 = TestProbe()
+    val roomRef = system.actorOf(Props(classOf[PokerRoomActor], "user-test-room"))
+    val userA = TestProbe()
+    val userB = TestProbe()
+    val userC = TestProbe()
 
     "ignore invalid messages" in {
       within(100 millis) {
@@ -30,27 +28,89 @@ class PokerRoomActorTest
     }
 
     "broadcast other participants when users join" in {
-      roomRef ! UserJoined("userA", probe1.ref)
-      roomRef ! UserJoined("userB", probe2.ref)
+      roomRef ! UserJoined("userA", userA.ref)
+      roomRef ! UserJoined("userB", userB.ref)
 
       // send other user
-      probe1.expectMsg(500 millis, UserJoined("userB", probe2.ref))
-      probe2.expectMsg(500 millis, UserJoined("userA", probe1.ref))
+      userA.expectMsg(UserJoined("userB", userB.ref))
+      userB.expectMsg(UserJoined("userA", userA.ref))
 
-      roomRef ! UserJoined("userC", probe3.ref)
+      roomRef ! UserJoined("userC", userC.ref)
       // broadcast new user
-      probe1.expectMsg(500 millis, UserJoined("userC", probe3.ref))
-      probe2.expectMsg(500 millis, UserJoined("userC", probe3.ref))
+      userA.expectMsg(UserJoined("userC", userC.ref))
+      userB.expectMsg(UserJoined("userC", userC.ref))
 
       // send other users to new user
-      probe3.expectMsg(500 millis, UserJoined("userA", probe1.ref))
-      probe3.expectMsg(500 millis, UserJoined("userB", probe2.ref))
+      userC.expectMsgAllOf(
+        UserJoined("userA", userA.ref),
+        UserJoined("userB", userB.ref)
+      )
     }
 
     "broadcast when users leave" in {
       roomRef ! UserLeft("userB")
-      probe1.expectMsg(500 millis, UserLeft("userB"))
-      probe3.expectMsg(500 millis, UserLeft("userB"))
+      userA.expectMsg(UserLeft("userB"))
+      userC.expectMsg(UserLeft("userB"))
+
+      roomRef ! UserLeft("userA")
+      userC.expectMsg(UserLeft("userA"))
+
+      roomRef ! UserLeft("userC")
+    }
+  }
+
+  "Estimation handling" should {
+    val roomRef = system.actorOf(Props(classOf[PokerRoomActor], "estimation-test-room"))
+    val userA = TestProbe()
+    val userB = TestProbe()
+    val userC = TestProbe()
+    val userD = TestProbe()
+
+    "setup" in {
+      roomRef ! UserJoined("userA", userA.ref)
+      roomRef ! UserJoined("userB", userB.ref)
+      roomRef ! UserJoined("userC", userC.ref)
+
+      userB.expectMsgAllOf(
+        UserJoined("userA", userA.ref),
+        UserJoined("userC", userC.ref)
+      )
+
+      userA.expectMsgAllOf(
+        UserJoined("userB", userB.ref),
+        UserJoined("userC", userC.ref)
+      )
+
+      userC.expectMsgAllOf(
+        UserJoined("userA", userA.ref),
+        UserJoined("userB", userB.ref)
+      )
+    }
+
+    "broadcast start of estimation" in {
+      roomRef ! RequestStartEstimation("userA", "new-task", "20150102T13:37:00")
+
+      val check: PartialFunction[Any, Boolean] = {
+        case RequestStartEstimation("userA", "new-task", _, _) => true
+      }
+
+      userA.expectMsgPF(500 millis)(check)
+      userB.expectMsgPF(500 millis)(check)
+      userC.expectMsgPF(500 millis)(check)
+    }
+
+    "should send current estimation to new user" in {
+      roomRef ! UserJoined("userD", userD.ref)
+
+      userD.expectMsgAllOf(
+        UserJoined("userA", userA.ref),
+        UserJoined("userB", userB.ref),
+        UserJoined("userC", userC.ref)
+      )
+
+      userD.expectMsgPF(500 millis)({
+        case RequestStartEstimation("", "new-task", _, _) => true
+      })
     }
   }
 
