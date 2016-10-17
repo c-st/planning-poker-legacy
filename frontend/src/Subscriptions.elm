e882ddc9766b8bdf82818b1bb9a33677ec69bba1
Christian Stangier <christian@stangier.email>
2016-10-17 21:25:44 +0200

    handle incoming keepAlive messages, subscribe to health checks

--- frontend/src/Subscriptions.elm
+++ frontend/src/Subscriptions.elm
@@ -1,7 +1,13 @@
 module Subscriptions exposing (subscriptions)
 
 import Globals exposing (planningPokerServerUrl)
-import Model exposing (Model, Msg, Msg(IncomingEvent, TimerTick), State(Estimate))
+import Model
+    exposing
+        ( Model
+        , Msg
+        , Msg(IncomingEvent, TimerTick, HealthCheckTick)
+        , State(Estimate)
+        )
 import WebSocket
 import Time exposing (second)
 import Platform.Sub exposing (none, batch)
@@ -27,5 +33,12 @@ subscriptions model =
                 Time.every second TimerTick
             else
                 Platform.Sub.none
+
+        healthCheckSubscription =
+            Time.every (2 * second) HealthCheckTick
     in
-        Sub.batch [ webSocketSubscription, timerTickSubscription ]
+        Sub.batch
+            [ webSocketSubscription
+            , timerTickSubscription
+            , healthCheckSubscription
+            ]
